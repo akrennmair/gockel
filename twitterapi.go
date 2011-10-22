0bd1419e7739af538d3a209d68ba33b917e079bf
Andreas Krennmair <ak@synflood.at>
2011-10-22 18:13:24 +0200

    oops. this should be HTTPS.

--- twitterapi.go
+++ twitterapi.go
@@ -93,9 +93,9 @@ type EventDetail struct {
 
 
 const (
-	request_token_url = "http://twitter.com/oauth/request_token"
-	access_token_url = "http://twitter.com/oauth/access_token"
-	authorization_url = "http://twitter.com/oauth/authorize"
+	request_token_url = "https://twitter.com/oauth/request_token"
+	access_token_url = "https://twitter.com/oauth/access_token"
+	authorization_url = "https://twitter.com/oauth/authorize"
 
 	INITIAL_NETWORK_WAIT int64 = 250e6 // 250 milliseconds
 	INITIAL_HTTP_WAIT    int64 = 10e9  // 10 seconds
